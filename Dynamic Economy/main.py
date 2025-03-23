18c3f8752374dc442f17ed115fc430b17fa162af
Joshua Giroux <joshua.d.giroux@gmail.com>
2025-03-23 15:51:04 -0400

    so tireddd

--- Dynamic Economy/main.py
+++ Dynamic Economy/main.py
@@ -16,9 +16,16 @@ class Econ:
 
     def econ_flux(self):
 
+        # soooo messy please clean up :(
+
         for x in items.tags:
 
-            num = 2 #random.randint(1,3)
+            num = random.randint(1,3)
+
+            # Refactor
+            # checking if the tag is in the updated_inventory
+            # if true, getting the old price and updating it to the new price
+            # writing that new price to updated_inventory.json
 
             if num == 1:
                 # price increase
@@ -67,7 +74,7 @@ class Items:
             "tag": self.tag
         }
 
-        items.inventory[self.item] = new_item
+        inventory[self.item] = new_item
 
         try:
             with open("og_inventory.json", "r") as f:
@@ -86,7 +93,7 @@ class Items:
             print("adding tag")
             items.tags.append(self.tag)
 
-    def all_items(self):
+    def show_all_items(self):
         for name, data in inventory.items():
             print(f"{name.title()}: {data['quantity']} units, {data['price']}gp [{data['tag']}]")
 
@@ -101,14 +108,9 @@ class Items:
         else:
             print(f"Items with {self.tag}: {tagged_items}")
 
-
-
-new_items = Items("Long Bow", 20, 20, "wood")
-# new_bow_items = Items("Short Bow", 20, 20, "wood")
-# axe = Items("Axe", 20, 20, 'steel')
-new_items.all_items()
+new_items = Items("Long Bow", 20, 20, "test")
+new_items.show_all_items()
 
 new_econ = Econ()
 new_econ.econ_flux()
-# new_items.show_items_with_tag('steel')
 
